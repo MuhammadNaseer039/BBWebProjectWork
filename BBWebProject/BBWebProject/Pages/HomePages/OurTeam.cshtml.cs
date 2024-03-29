320b55a8399e2455762408b1668808d718a645f7
Muhammad Naseer <naseershabbir954@gmail.com>
2024-03-29 11:33:54 -0700

    Pages Added
    All relevant pages are added

---
+++ BBWebProject/BBWebProject/Pages/HomePages/OurTeam.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BBWebProject.Pages.HomePages
+{
+    public class OurTeamModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ BBWebProject/BBWebProject/Pages/HomePages/Service.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BBWebProject.Pages.HomePages
+{
+    public class ServiceModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
