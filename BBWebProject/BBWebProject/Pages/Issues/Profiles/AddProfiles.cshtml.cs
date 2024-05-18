c860c4a323fda8cc874e393a4eac0513646d3ce0
Muhammad Naseer <naseershabbir954@gmail.com>
2024-05-18 14:32:24 -0700

    Updating
    Some changings in Template and Update things

---
+++ BBWebProject/BBWebProject/Pages/Issues/Profiles/AddProfiles.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BBWebProject.Pages.Issues.Profiles
+{
+    public class AddProfilesModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ BBWebProject/BBWebProject/Pages/Issues/Profiles/UpdateProfiles.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BBWebProject.Pages.Issues.Profiles
+{
+    public class UpdateProfilesModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
