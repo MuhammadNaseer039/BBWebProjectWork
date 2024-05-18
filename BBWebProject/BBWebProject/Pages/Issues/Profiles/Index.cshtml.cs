74b1853d41b8808a020f51b13ca39dd0dff7feb7
Muhammad Naseer <naseershabbir954@gmail.com>
2024-05-18 15:06:47 -0700

    Profile Module
    Profile Module Completed

--- BBWebProject/BBWebProject/Pages/Issues/Profiles/Index.cshtml.cs
+++ BBWebProject/BBWebProject/Pages/Issues/Profiles/Index.cshtml.cs
@@ -1,3 +1,5 @@
+using BBWebProject.Data;
+using BBWebProject.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +7,24 @@ namespace BBWebProject.Pages.Issues.Profiles
 {
     public class IndexModel : PageModel
     {
+        private readonly BBWebDbContext db;
+        public List<Profile> profiles { get; set; }
+        public Profile profile { get; set; }
+        public IndexModel(BBWebDbContext _db)
+        {
+            db = _db;
+        }
         public void OnGet()
         {
+            profiles = db.tbl_profile.ToList();
         }
+        public IActionResult OnPostDelete(int id)
+        {
+            profile = db.tbl_profile.Find(id);
+            db.tbl_profile.Remove(profile);
+            db.SaveChanges();
+            return RedirectToPage("Index");
+        }
+
     }
 }
