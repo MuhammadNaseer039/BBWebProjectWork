8e060f06425eb7327edcf01faa04c2768e3ab4e2
Muhammad Naseer <naseershabbir954@gmail.com>
2024-05-29 03:10:37 -0700

    Slug Implementation
    In this pull request I have implemented slug

--- BBWebProject/BBWebProject/Pages/HomePages/Service.cshtml.cs
+++ BBWebProject/BBWebProject/Pages/HomePages/Service.cshtml.cs
@@ -5,8 +5,9 @@ namespace BBWebProject.Pages.HomePages
 {
     public class ServiceModel : PageModel
     {
-        public void OnGet()
+        public void OnGet(string slug)
         {
+
         }
     }
 }
